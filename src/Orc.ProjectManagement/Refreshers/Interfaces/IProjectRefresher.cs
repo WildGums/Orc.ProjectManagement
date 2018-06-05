071bd3e11efa2c30eacf8c747dd74fa7c2cc9dfd
Maksim Khomutov <mkhomutov@gmail.com>
2018-06-05 19:08:59 +0300

    RT-398 allow to disable IProjectRefresher

--- src/Orc.ProjectManagement/Refreshers/Interfaces/IProjectRefresher.cs
+++ src/Orc.ProjectManagement/Refreshers/Interfaces/IProjectRefresher.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="IProjectRefresher.cs" company="WildGums">
-//   Copyright (c) 2008 - 2014 WildGums. All rights reserved.
+//   Copyright (c) 2008 - 2018 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -11,12 +11,17 @@ namespace Orc.ProjectManagement
 
     public interface IProjectRefresher
     {
+        #region Properties
         string Location { get; }
         bool IsSubscribed { get; }
+        bool IsEnabled { get; set; }
+        #endregion
 
+        #region Methods
         event EventHandler<ProjectEventArgs> Updated;
 
         void Subscribe();
         void Unsubscribe();
+        #endregion
     }
 }
--- src/Orc.ProjectManagement/Refreshers/ProjectRefresherBase.cs
+++ src/Orc.ProjectManagement/Refreshers/ProjectRefresherBase.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ProjectRefresherBase.cs" company="WildGums">
-//   Copyright (c) 2008 - 2014 WildGums. All rights reserved.
+//   Copyright (c) 2008 - 2018 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -16,14 +16,20 @@ namespace Orc.ProjectManagement
 
     public abstract class ProjectRefresherBase : IProjectRefresher
     {
+        #region Fields
         private static readonly ILog Log = LogManager.GetCurrentClassLogger();
 
         // Note: dirty solution, but IProjectRefresherSelector is injected into ICommandManager, 
         // so we cannot accept ICommandManager in there (circular reference)
         private readonly Lazy<IProjectManager> _projectManager = new Lazy<IProjectManager>(() => ServiceLocator.Default.ResolveType<IProjectManager>());
+        private bool _isSuspended;
+        #endregion
 
+        #region Constructors
         protected ProjectRefresherBase(string projectLocation)
-            : this(projectLocation, projectLocation) { }
+            : this(projectLocation, projectLocation)
+        {
+        }
 
         protected ProjectRefresherBase(string projectLocation, string locationToWatch)
         {
@@ -33,24 +39,26 @@ namespace Orc.ProjectManagement
             ProjectLocation = projectLocation;
             Location = locationToWatch;
         }
-
-        #region Properties
-        protected IProjectManager ProjectManager { get { return _projectManager.Value; } }
-
-        public string ProjectLocation { get; private set; }
-
-        public string Location { get; private set; }
-
-        public bool IsSubscribed { get; private set; }
-
-        public bool IsSuspended { get; private set; }
         #endregion
 
+        #region Properties
+        protected IProjectManager ProjectManager => _projectManager.Value;
+
+        public string ProjectLocation { get; }
+
+        public string Location { get; }
+
+        public bool IsSubscribed { get; private set; }
+        public bool IsEnabled { get; set; }
+
+        public bool IsSuspended => !IsEnabled || _isSuspended;
+        #endregion
+
+        #region Methods
         #region Events
         public event EventHandler<ProjectEventArgs> Updated;
         #endregion
 
-        #region Methods
         public void Subscribe()
         {
             var location = Location;
@@ -72,8 +80,6 @@ namespace Orc.ProjectManagement
 
             IsSubscribed = true;
         }
-
-        protected abstract void SubscribeToLocation(string location);
 
         public void Unsubscribe()
         {
@@ -97,6 +103,8 @@ namespace Orc.ProjectManagement
             IsSubscribed = false;
         }
 
+        protected abstract void SubscribeToLocation(string location);
+
         protected abstract void UnsubscribeFromLocation(string location);
 
         protected void RaiseUpdated(string fileName)
@@ -106,28 +114,28 @@ namespace Orc.ProjectManagement
 
         private Task OnProjectManagerSavingAsync(object sender, ProjectCancelEventArgs e)
         {
-            IsSuspended = true;
+            _isSuspended = true;
 
             return TaskHelper.Completed;
         }
 
         private Task OnProjectManagerSavedAsync(object sender, ProjectEventArgs e)
         {
-            IsSuspended = false;
+            _isSuspended = false;
 
             return TaskHelper.Completed;
         }
 
         private Task OnProjectManagerSavingCanceledAsync(object sender, ProjectEventArgs e)
         {
-            IsSuspended = false;
+            _isSuspended = false;
 
             return TaskHelper.Completed;
         }
 
         private Task OnProjectManagerSavingFailedAsync(object sender, ProjectErrorEventArgs e)
         {
-            IsSuspended = false;
+            _isSuspended = false;
 
             return TaskHelper.Completed;
         }
