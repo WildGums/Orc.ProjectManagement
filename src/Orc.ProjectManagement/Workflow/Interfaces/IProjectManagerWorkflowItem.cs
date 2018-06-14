b748dd7252db7f0d05b3a47dabc1da5f59b2fe2f
Maksim Khomutov <mkhomutov@gmail.com>
2018-06-15 00:45:16 +0300

    FG-311 Add ProjectManagementWorkflow infrastructure

---
+++ src/Orc.ProjectManagement/Workflow/Interfaces/IProjectManagerWorkflowItem.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IProjectManagerWorkflowItem.cs" company="WildGums">
+//   Copyright (c) 2008 - 2018 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.ProjectManagement
+{
+    using System;
+    using System.Threading.Tasks;
+    using Catel.Data;
+
+    /// <summary>
+    /// This is the replacement of all the already implemented ProjectWatcher instances
+    /// The interface and its implementations will be deprecated in next stage of getting rid of ProjectWatcher conception
+    /// </summary>
+    public interface IProjectManagerWorkflowItem
+    {
+        #region Methods
+        Task<bool> LoadingAsync(string location);
+        Task LoadingCanceledAsync(string location);
+        Task LoadingFailedAsync(string location, Exception exception, IValidationContext validationContext);
+        Task LoadedAsync(IProject project);
+        Task<bool> ActivationAsync(IProject oldProject, IProject newProject, bool isRefresh);
+        Task ActivationCanceledAsync(IProject project);
+        Task ActivationFailedAsync(IProject project, Exception exception);
+        Task ActivatedAsync(IProject oldProject, IProject newProject);
+        Task<bool> ClosingAsync(IProject project);
+        Task ClosingCanceledAsync(IProject project);
+        Task ClosedAsync(IProject project);
+        Task<bool> RefreshingAsync(IProject project);
+        Task RefreshingCanceledAsync(IProject project);
+        Task RefreshingFailedAsync(IProject project, Exception exception, IValidationContext validationContext);
+        Task RefreshedAsync(IProject project);
+        Task<bool> SavingAsync(IProject project);
+        Task SavingCanceledAsync(IProject project);
+        Task SavingFailedAsync(IProject project);
+        Task SavedAsync(IProject project);
+        #endregion
+    }
+}
