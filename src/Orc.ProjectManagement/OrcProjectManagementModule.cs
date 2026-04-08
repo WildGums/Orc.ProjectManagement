dbdcabb56c2db9594810c0da3b81a47b78ef4047
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:28:45 +0200

    Convert to file scoped namespaces

--- src/Orc.ProjectManagement/OrcProjectManagementModule.cs
+++ src/Orc.ProjectManagement/OrcProjectManagementModule.cs
@@ -1,38 +1,37 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Catel.ThirdPartyNotices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Orc.ProjectManagement;
+using Orc.ProjectManagement.Serialization;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcProjectManagementModule
 {
-    using Catel.Services;
-    using Catel.ThirdPartyNotices;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Orc.ProjectManagement;
-    using Orc.ProjectManagement.Serialization;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcProjectManagementModule
+    public static IServiceCollection AddOrcProjectManagement(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcProjectManagement(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.TryAddSingleton<IProjectManager, ProjectManager>();
-            serviceCollection.TryAddSingleton<IProjectStateService, ProjectStateService>();
-            serviceCollection.TryAddSingleton<IProjectInitializer, EmptyProjectInitializer>();
-            serviceCollection.TryAddSingleton<IProjectValidator, EmptyProjectValidator>();
-            serviceCollection.TryAddSingleton<IProjectUpgrader, EmptyProjectUpgrader>();
-            serviceCollection.TryAddSingleton<IProjectRefresherSelector, DefaultProjectRefresherSelector>();
-            serviceCollection.TryAddSingleton<IProjectActivationHistoryService, ProjectActivationHistoryService>();
-            serviceCollection.TryAddSingleton<IInitialProjectLocationService, InitialProjectLocationService>();
+        serviceCollection.TryAddSingleton<IProjectManager, ProjectManager>();
+        serviceCollection.TryAddSingleton<IProjectStateService, ProjectStateService>();
+        serviceCollection.TryAddSingleton<IProjectInitializer, EmptyProjectInitializer>();
+        serviceCollection.TryAddSingleton<IProjectValidator, EmptyProjectValidator>();
+        serviceCollection.TryAddSingleton<IProjectUpgrader, EmptyProjectUpgrader>();
+        serviceCollection.TryAddSingleton<IProjectRefresherSelector, DefaultProjectRefresherSelector>();
+        serviceCollection.TryAddSingleton<IProjectActivationHistoryService, ProjectActivationHistoryService>();
+        serviceCollection.TryAddSingleton<IInitialProjectLocationService, InitialProjectLocationService>();
 
-            serviceCollection.TryAddSingleton<IProjectSerializerSelector, DefaultProjectSerializerSelector>();
-            serviceCollection.TryAddSingleton<IProjectManagementConfigurationService, SdiProjectManagementConfigurationService>();
-            serviceCollection.TryAddSingleton<IProjectManagementInitializationService, ProjectManagementInitializationService>();
+        serviceCollection.TryAddSingleton<IProjectSerializerSelector, DefaultProjectSerializerSelector>();
+        serviceCollection.TryAddSingleton<IProjectManagementConfigurationService, SdiProjectManagementConfigurationService>();
+        serviceCollection.TryAddSingleton<IProjectManagementInitializationService, ProjectManagementInitializationService>();
 
 
-            serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.ProjectManagement", "Orc.ProjectManagement.Properties", "Resources"));
+        serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.ProjectManagement", "Orc.ProjectManagement.Properties", "Resources"));
 
-            serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.ProjectManagement", "https://github.com/wildgums/orc.projectmanagement"));
+        serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.ProjectManagement", "https://github.com/wildgums/orc.projectmanagement"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
